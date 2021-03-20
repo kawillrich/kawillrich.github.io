4b3eeb832078a96c441d5f52a34fd14e452cabb2
kawillrich <66137285+kawillrich@users.noreply.github.com>
2021-03-19 22:15:08 -0400

    Add files via upload

---
+++ js/armor-class.js
@@ -0,0 +1,9 @@
+export default class Armor {
+    constructor (name, armorPoints) {
+        this.name = name;
+        this.armorPoints = armorPoints;
+       
+    }
+};
+
+
---
+++ js/character-class.js
@@ -0,0 +1,12 @@
+export default class Character {
+    constructor(name, specialty, armor, weapon) {
+        this.name = name;
+        this.specialty = specialty;
+        this.armor = armor;
+        this.weapon = weapon;
+        
+     
+
+    };
+};
+
