1698aa4945686ab357275e282820284bef01e761
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-01-07 20:20:05 -0500

    Update inventory-array-v1.js
    adding items

--- js/inventory-array-v1.js
+++ js/inventory-array-v1.js
@@ -1,5 +1,16 @@
 export let inventory = [];
 
+
+/* 
+export let  = { 
+    name: "",
+    description: "",
+    enchantment: 0,
+    encumbrance: 0,
+}
+*/
+
+
 export let raynardsCoin1 = {
     name: "Raynard's Coin", 
     description: "Coin provided to you from Raynard", 
@@ -9,21 +20,104 @@ export let raynardsCoin1 = {
 export let farmersNote1 = {
     name: "Farmer's Note", 
     description: "Note received from farmers after defeating wolves", 
-    enchantment: 0
+    enchantment: 0,
+    encumbrance: 0
 };
 
 export let eloisesRing1 = {
     name: "Eloise's Ring", 
     description: "Ring Eloise wore", 
-    enchantment: 0
+    enchantment: 0,
+    encumbrance: 0
 };
 
 export let noItem1 = {
-    name: "Eloise's Ring", 
-    description: "Ring Eloise wore", 
-    enchantment: 0
+    name: "None", 
+    description: "None", 
+    enchantment: 0,
+    encumbrance: 0
 };
 
+export let torch = {
+    name: "Torch",
+    description: "Ordinary Torch",
+    enchantment: 0,
+    encumbrance: 1,
+}
 
+export let backPack = {
+    name: "Backpack",
+    description: "Ordinary backpack",
+    enchantment: 0,
+    encumbrance: 20,
+}
 
+export let holySymbol = { 
+    name: "Holy Symbol",
+    description: "Holy Symbol for clerics",
+    enchantment: 0,
+    encumbrance: 1,
+}
 
+export let holyWater = { 
+    name: "Holy Water",
+    description: "1 vial of Holy Water for clerics",
+    enchantment: 0,
+    encumbrance: 1,
+}
+
+export let smallHammer = { 
+    name: "Small Hammer",
+    description: "Ordinary small hammer",
+    enchantment: 0,
+    encumbrance: 10,
+}
+
+export let ironSpikes = { 
+    name: "Iron Spikes",
+    description: "Iron Spikes",
+    enchantment: 0,
+    encumbrance: 5,
+}
+
+export let garlic = { 
+    name: "Garlic",
+    description: "Bulb of garlic",
+    enchantment: 0,
+    encumbrance: 1,
+}
+
+export let grapplingHook = { 
+    name: "Grappling Hook",
+    description: "Ordinary Grappling Hook",
+    enchantment: 0,
+    encumbrance: 80,
+}
+
+export let lantern = { 
+    name: "Lantern",
+    description: "Ordinary Lantern",
+    enchantment: 0,
+    encumbrance: 30,
+}
+
+export let mirrorHandSized = { 
+    name: "Mirror (hand-sized, steel)",
+    description: "Hand-sized mirror made of steel",
+    enchantment: 0,
+    encumbrance: 5,
+}
+
+export let oil = { 
+    name: "Oil",
+    description: "One flask of oil",
+    enchantment: 0,
+    encumbrance: 10,
+}
+
+export let  = { 
+    name: "",
+    description: "",
+    enchantment: 0,
+    encumbrance: 0,
+}
