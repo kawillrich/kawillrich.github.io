e8a993d2a296788c358577ca67822909004b3a76
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-01-15 17:50:49 -0500

    imported mage levels

--- js/specialty-class-v2.js
+++ js/specialty-class-v2.js
@@ -1,4 +1,5 @@
 import { fighterWarrior, fighterVeteran, fighterSwordmaster } from './fighter-level-class.js';
+import { mageMedium, mageSeer, mageConjurer } from './mage-level-class.js';
 import { noSpell, mediumHealing, majorHealing, earthStrike, lightening, minorHealing, fireBall, fireArrows, dragonFang} from './spell-class-v2.js';
 export default class Specialty {
     constructor(name, healthPoints, spell1, spell2, spell3, maxHealthPoints, characterExperience, characterLanguages, characterLevel) {
@@ -21,7 +22,7 @@ let warrior = new Specialty('Warrior', 140, noSpell, noSpell, noSpell, 140, 0, [
 
 let masterArcher = new Specialty('Master Archer', 125, minorHealing, noSpell, noSpell, 125, 0, ["Common Tongue"], );
 
-let highMage = new Specialty('High Mage', 115, minorHealing, fireBall, fireArrows, 115, 0, ["Common Tongue"], );
+let highMage = new Specialty('High Mage', 115, minorHealing, fireBall, fireArrows, 115, 0, ["Common Tongue"], mageMedium);
 
 let dragonWarrior = new Specialty('Dragon Warrior', 500, majorHealing, dragonFang, dragonFang, 500, 0, ["Common Tongue"], )
 
