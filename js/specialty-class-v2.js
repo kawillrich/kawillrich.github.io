e4756e5b5ce4e1b3225e6110e61d8b6b7d0e5d55
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-01-15 14:02:47 -0500

    Update specialty-class-v2.js
    added char saving throws and hit roll

--- js/specialty-class-v2.js
+++ js/specialty-class-v2.js
@@ -1,6 +1,6 @@
 import { noSpell, mediumHealing, majorHealing, earthStrike, lightening, minorHealing, fireBall, fireArrows, dragonFang} from './spell-class-v2.js';
 export default class Specialty {
-    constructor(name, healthPoints, spell1, spell2, spell3, maxHealthPoints, characterExperience, characterLanguages, characterLevel) {
+    constructor(name, healthPoints, spell1, spell2, spell3, maxHealthPoints, characterExperience, characterLanguages, characterLevel, characterSavingThrows, characterHitRoll) {
         this.name = name;
         this.healthPoints = healthPoints;
         this.spell1 = spell1;
@@ -10,6 +10,8 @@ export default class Specialty {
         this.chacterLanguages = characterLanguages;
         this.characterLevel = characterLevel || 1;
         this.characterExperience = characterExperience || 0;
+        this.characterSavingThrows = characterSavingThrows;
+        this.characterHitRoll = characterHitRoll;
         
     };
 };
