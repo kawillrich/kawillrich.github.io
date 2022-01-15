405f30f8739034861f83bf4167a07a1aa3269a5d
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-01-15 17:44:41 -0500

    Update mage-level-one-spells-class.js
    added spells

--- js/mage-level-one-spells-class.js
+++ js/mage-level-one-spells-class.js
@@ -10,5 +10,18 @@ export default class MageLevelOneSpells {
 };
 
 let charmPerson = new MageLevelOneSpells ("Charm Person", 120, "Varies", "Charms one person");
+let detectMagic = new MageLevelOneSpells ("Detec Magic", 0, 2, "Detects magic within 60 feet");
+let floatingDisc = new MageLevelOneSpells ("Floating Disc", 0, 6, "Creates an invisible disc that can carry 5000 cn");
+let holdPortal = new MageLevelOneSpells ("Hold Portal", 10, [2,12], "On door, gate, or similar portal");
+let lightSpell = new MageLevelOneSpells ("Light", 120, 6, "Volume of 30 feet diameter");
+let magicMissile = new MageLevelOneSpells ("Magic Missile", 150, 0.1, "Creates one or more arrows");
+let protectionFromEvil = new MageLevelOneSpells ("Protection from Evil", 0, 6, "The mage only");
+let readLanguages = new MageLevelOneSpells ("Read Languages", 0, 2, "The mage only");
+let shieldSpell = new MageLevelOneSpells ("Shield", 0, 2, "The mage only");
+let sleepSpell = new MageLevelOneSpells ("Sleep", 240, [4, 16], "2-16 Hit Dice of leaving creatures within a 40 foot square area");
+let ventriloquism = new MageLevelOneSpells ("Ventriloquism", 60, 2, "One item or location");
+let continualLight = new MageLevelOneSpells ("Continual Light", 120, 999, "Volume of 60 foot diameter");
+let detectEvil = new MageLevelOneSpells ("Detect Evil", 60, 2, "Everything within 60 feet");
+let invisibility = new MageLevelOneSpells ("Invisibility", 240, 999, "One creature or object");
 
-export { charmPerson };
+export { charmPerson, detectMagic, floatingDisc, holdPortal, lightSpell, magicMissile, protectionFromEvil, readLanguages, shieldSpell, sleepSpell, ventriloquism, continualLight, detectEvil, invisibility };
