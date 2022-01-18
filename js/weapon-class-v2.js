0acf7e081205edb94707b070a69caea663d9aeef
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-01-18 15:01:48 -0500

    Update weapon-class-v2.js
    added numOfHands

--- js/weapon-class-v2.js
+++ js/weapon-class-v2.js
@@ -1,22 +1,24 @@
 
 export default class Weapon {
-    constructor (name, damage, type) {
+    constructor (name, damage, type, numOfHands) {
         this.name = name;
         this.damage = damage;
+        this.type = type;
+        this.numOfHands = numOfHands;
     }
 
    
 }
- let steelSword = new Weapon('Steel Sword', 8, 'Melee');
- let shortBow = new Weapon('Short Bow', 6, 'Missile');
- let staff = new Weapon('Staff', 4, 'Melee');
- let silverSword = new Weapon('Silver Sword', 10, 'Melee');
- let twoHandedBroadSword = new Weapon('Two-Handed Sword', 12, 'Melee');
- let longBow = new Weapon('Long Bow', 8, 'Missile');
- let ebonyBow = new Weapon('Ebony Bow', 10, 'Missile');
- let mahoganyStaff = new Weapon('Mahogany Staff', 6, 'Melee');
- let gemStaff = new Weapon('Gem Staff', 8, 'Melee');
+ let steelSword = new Weapon('Steel Sword', 8, 'Melee', 1);
+ let shortBow = new Weapon('Short Bow', 6, 'Missile', 2);
+ let staff = new Weapon('Staff', 4, 'Melee', 1);
+ let silverSword = new Weapon('Silver Sword', 10, 'Melee', 1);
+ let twoHandedBroadSword = new Weapon('Two-Handed Sword', 12, 'Melee', 2);
+ let longBow = new Weapon('Long Bow', 8, 'Missile', 2);
+ let ebonyBow = new Weapon('Ebony Bow', 10, 'Missile', 2);
+ let mahoganyStaff = new Weapon('Mahogany Staff', 6, 'Melee', 1);
+ let gemStaff = new Weapon('Gem Staff', 8, 'Melee', 1);
  let noWeapon = new Weapon('None', 0);
- let obsidianSword = new Weapon('Obsidian Sword', 100, 'Melee');
+ let obsidianSword = new Weapon('Obsidian Sword', 100, 'Melee', 1);
 
 export { steelSword, shortBow, staff, silverSword, twoHandedBroadSword, longBow, ebonyBow, mahoganyStaff, gemStaff, noWeapon, obsidianSword };
