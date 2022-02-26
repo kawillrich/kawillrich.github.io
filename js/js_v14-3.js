09c49ea6fe134624fb51baee64df4b21da46cb15
kawillrich <66137285+kawillrich@users.noreply.github.com>
2022-02-25 20:11:04 -0500

    added mage level 2 and 3 spell tooltips

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -983,39 +983,55 @@ function submitAllMageSpells() {
             addedSpell.appendChild(addedSpellTextNode);
             addedSpell.appendChild(addedSpellSpan);
 
-            console.log(addedSpell);
-
             let spellList = document.querySelector('#spells-level-1');
-            console.log(spellList);
             spellList.appendChild(addedSpell);
-            console.log(addedSpell.innerHTML);
         }
 
         for (let i = 0; i < finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"].length; i++) {            
 
             let addedSpell = document.createElement('li');
+            addedSpell.classList.add('spell-tooltip');
+
+            let addedSpellSpan = document.createElement('span');
+            addedSpellSpan.innerHTML = 
+            `<span class="spell-tooltiptext">Range: ${finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"][i].range}<br>
+            Duration: ${finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"][i].duration}<br>
+            Effect: ${finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"][i].effect}</span>
+            `;
+
             let newID = finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"][i].className;
             addedSpell.setAttribute('id', newID);
             let newTextName = finalCharacter.specialty.characterLevel.specialtySkills["Second Level Mage Spells"][i].name;
             let addedSpellTextNode = document.createTextNode(newTextName);
             addedSpell.appendChild(addedSpellTextNode);
-            console.log(addedSpell);
+            addedSpell.appendChild(addedSpellSpan);
+
             let spellList = document.querySelector('#spells-level-2');
-            console.log(spellList);
             spellList.appendChild(addedSpell);
         }
 
         for (let i = 0; i < finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"].length; i++) {            
 
             let addedSpell = document.createElement('li');
+            addedSpell.classList.add('spell-tooltip');
+
+            let addedSpellSpan = document.createElement('span');
+            addedSpellSpan.innerHTML = 
+            `<span class="spell-tooltiptext">Range: ${finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"][i].range}<br>
+            Duration: ${finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"][i].duration}<br>
+            Effect: ${finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"][i].effect}</span>
+            `;
+
+
             let newID = finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"][i].className;
-            addedSpell.setAttribute('id', newID);
+            addedSpell.setAttribute('id', newID);            
             let newTextName = finalCharacter.specialty.characterLevel.specialtySkills["Third Level Mage Spells"][i].name;
             let addedSpellTextNode = document.createTextNode(newTextName);
             addedSpell.appendChild(addedSpellTextNode);
-            console.log(addedSpell);
+            addedSpell.appendChild(addedSpellSpan);
+
+
             let spellList = document.querySelector('#spells-level-3');
-            console.log(spellList);
             spellList.appendChild(addedSpell);
         }
 
