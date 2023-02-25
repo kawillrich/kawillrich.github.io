de63d60a9f7fb5847c65d447f9396274127abdc6
kawillrich <66137285+kawillrich@users.noreply.github.com>
2023-02-25 16:55:09 -0500

    chore: deleted commented out code in secondlevel mage spell

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -2920,18 +2920,7 @@ function castSpellFromList(e, m1, m2, nextChap)
     {
       if (secondLevel[i].name === parentNodeInnerText)
       {
-
         secondLevel[i].castSpell(m1, m2, nextChap, thisNodeInnerText);
-
-        // if (thisNodeInnerText === "Monster 1")
-        // {
-        //   secondLevel[i].castingEffect(m1);
-
-        // } else if (thisNodeInnerText === "Monster 2")
-        // {
-        //   secondLevel[i].castingEffect(m2);
-        // }
-
       }
     };
 
