695281041734eede676c660e957360f64032b09a
kawillrich <66137285+kawillrich@users.noreply.github.com>
2023-03-04 16:58:38 -0500

    fix: corrected attackMonster1 to m2 in ventriloquism

--- js/mage-spells/mage-level-one-spells-class.js
+++ js/mage-spells/mage-level-one-spells-class.js
@@ -109,7 +109,7 @@ ventriloquism.castSpell = function (monster1, monster2, continueNextChapter, att
             toggleShowSpellList();
             if (confirm(`You cast Ventriloquism, and cause a distraction and the ${monster2.name} is caught offguard increasing your chances of hitting, do you want to take the initiative and attack the ${monster2.name}?`))
             {
-                finalCharacter.weaponAttackMonster1(monster1, monster2, finalCharacter.weapon, continueNextChapter)
+                finalCharacter.weaponAttackMonster2(monster1, monster2, finalCharacter.weapon, continueNextChapter, 2, 2)
             } else
             {
                 dialogue.innerHTML = `<p>You cast Ventriloquism, and cause a distraction and the ${monster2.name} is caught offguard, but you decide not to attack</p>`;
