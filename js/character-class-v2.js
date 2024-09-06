02d2e8d8dd2a9a444d22d07920d34b705bdf7cf7
kawillrich <66137285+kawillrich@users.noreply.github.com>
2024-09-06 13:01:09 -0400

    feat: add displaying saves for char sheet

--- js/character-class-v2.js
+++ js/character-class-v2.js
@@ -405,11 +405,11 @@ export default class Character
         <div id="character-saving-throws">
           <fieldset class = "saves-info-module-player">
             <legend class="saves-dashboard">Saves</legend>
-              <h4>Poison or Death Ray: ${finalCharacter.specialty.characterLevel.savingThrows}</h4>
-              <h4>Magic Wand: </h4>
-              <h4>Turn to Stone or Paralysis: </h4>
-              <h4>Dragon Breath</h4>
-              <h4>Spells or Magic Staff</h4>
+              <h4>Poison or Death Ray: ${finalCharacter.specialty.characterLevel.level.savingThrows["Poison or Death Ray"]}</h4>
+              <h4>Magic Wand: ${finalCharacter.specialty.characterLevel.level.savingThrows["Magic Wand"]}</h4>
+              <h4>Turn to Stone or Paralysis: ${finalCharacter.specialty.characterLevel.level.savingThrows["Turn to Stone or Paralysis"]}</h4>
+              <h4>Dragon Breath: ${finalCharacter.specialty.characterLevel.level.savingThrows["Dragon Breath"]}</h4>
+              <h4>Spells or Magic Staff: ${finalCharacter.specialty.characterLevel.level.savingThrows["Spells or Magic Staff"]}</h4>
           </fieldset>
         </div>             
         `
