8a173ab4e877e68da9818e75e74e54192e962adb
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-01-17 21:09:00 -0500

    style: place background circle-dex in dex

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -1039,10 +1039,10 @@ function startGame() {
                 <span class='character-display-attributes-scores-adj-dex'>${dexterityPlusAdjustment}</span>
                 
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100">    
+                  
                   <circle class="circle-dex-background" cx="50" cy="50" r="30"/>
 
                   <circle class="circle-dex" cx="50" cy="50" r="30" stroke-dashoffset="${dexterity.dashArrayAdj()}"/>
-
                 </svg>                
 
               </h4>
